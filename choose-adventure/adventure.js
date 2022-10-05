342adda1683a255666be95d25a1430501be77015
katherine rodriguez <katherine.rodriguezz.ny@gmail.com>
2022-10-04 22:50:35 -0400

    second commit

---
+++ choose-adventure/adventure.js
@@ -0,0 +1,22 @@
+//start with a prompt, use an if statement to
+ //show different messages based on what the user 
+// enters in the prompt
+
+
+
+
+
+let place = prompt("type 'Paris' or 'No Paris'");
+
+if (place === 'Paris') {
+    //You fly to Paris
+    alert('Woo! Lets Fly To Paris!');
+
+} else if (place === 'No Paris' ){
+//You do not fly to Paris
+}
+else{
+    alert('Well that is the end of your journey, womp womp')
+}
+
+
