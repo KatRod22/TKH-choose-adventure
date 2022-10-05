90aed48458375e76e0ddd53ae31d05db43875876
katherine rodriguez <katherine.rodriguezz.ny@gmail.com>
2022-10-04 23:18:19 -0400

    third commit

--- choose-adventure/adventure.js
+++ choose-adventure/adventure.js
@@ -8,15 +8,43 @@
 
 let place = prompt("type 'Paris' or 'No Paris'");
 
-if (place === 'Paris') {
+if (place === 'Paris.toLowerCase()') {
     //You fly to Paris
     alert('Woo! Lets Fly To Paris!');
 
-} else if (place === 'No Paris' ){
+} else if (place === 'No Paris.toLowerCase()' ){
 //You do not fly to Paris
 }
 else{
     alert('Well that is the end of your journey, womp womp')
 }
 
+let food = prompt("type 'croissant' or 'no croissant'");
+
+if (food === 'croissant.toLowerCase()') {
+    //Will eat croissant
+    alert('yum, lets go enjoy a croissant!');
+}
+
+else if (food === 'No croissant.toLowerCase()') {
+    //You do not eat a croissant
+}
+
+else{
+    alert('What?! Who does not eat a croissant in paris?');
+}
+
+let attraction = prompt("type 'Eiffel' or 'no Eiffel'");
+
+if (attraction === 'Eiffel.toLowerCase()') {
+    //will visit Eiffel Tower
+}
+
+else if (attraction === 'no Eiffel.toLowerCase()') {
+//will not visit Eiffel Tower
+}
+
+else{
+    alert('NO EIFFEL TOWER?! SO WHY ARE WE IN PARIS?')
+}
 
